71c8d73adc5a17e9388a4010d286405f611e44d1
stonelead <157927691+stonelead@users.noreply.github.com>
2025-05-25 22:51:57 +0300

    Update tools.ps1

--- scripts/tools.ps1
+++ scripts/tools.ps1
@@ -3,23 +3,23 @@ choco install notepadplusplus -y
 
 # Define the URL and local path for the Temurin JRE installer
 $TemurinUrl = "https://api.adoptium.net/v3/installer/latest/24/ga/windows/x64/jre/hotspot/normal/eclipse"
-$TemurinMsiPath = "$env:TEMP\temurin23.msi"
+$TemurinMsiPath = "$env:TEMP\temurin24.msi"
 
-Write-Host "Downloading and installing Temurin 23 JRE..."
+Write-Host "Downloading and installing Temurin 24 JRE..."
 
 try {
     Invoke-WebRequest -Uri $TemurinUrl -OutFile $TemurinMsiPath
     Write-Host "Download completed successfully."
 } catch {
-    Write-Error "Failed to download Temurin 23 JRE: $_"
+    Write-Error "Failed to download Temurin 24 JRE: $_"
     exit 1
 }
 
 try {
     Start-Process msiexec.exe -ArgumentList "/i $TemurinMsiPath /quiet /norestart" -NoNewWindow -Wait
-    Write-Host "Temurin 23 JRE installed successfully."
+    Write-Host "Temurin 24 JRE installed successfully."
 } catch {
-    Write-Error "Failed to install Temurin 23 JRE: $_"
+    Write-Error "Failed to install Temurin 24 JRE: $_"
     exit 1
 }
 
